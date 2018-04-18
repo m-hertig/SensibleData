cbed3eacbb8f23293ad7ba8e6d6e728224070809
m-hertig <martin.hertig@gmail.com>
2018-04-18 07:03:26 +0000

    add start and install scripts

---
+++ install.sh
@@ -0,0 +1,12 @@
+# install dependencies. run as root
+apt-get install libjpeg8-dev
+pip install unicodedata
+pip install requests
+pip install configobj
+pip install pillow
+pip install awscli
+pip install certifi
+pip install boto3
+pip install flask_cors
+pip install elasticsearch
+apt-get install python-flask
---
+++ startSensibleData.sh
@@ -0,0 +1,7 @@
+# Start all sensible data scripts
+# tap 'sudo crontab -e & put the following line in there'
+# @reboot /home/pi/SensibleData/startSensibleData.sh
+
+#/usr/bin/python /home/pi/SensibleData/PiccoloStampSimple.py &
+#/usr/bin/python /home/pi/SensibleData/PiccoloStampRekognition.py &
+#/usr/local/bin/watchmedo shell-command --patterns="*.jpg" --command='flock -n /home/pi/SensibleData/drawportrait.lockfile -c "/usr/bin/python /home/pi/SensibleData/DrawPortrait.py"'  /home/pi/SensibleData/images/ &
